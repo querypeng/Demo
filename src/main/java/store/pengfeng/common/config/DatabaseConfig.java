78b23232b067c5008fd753cd870204ac853a9410
sensen <1107474494@qq.com>
2018-12-24 17:53:50 +0800

    项目优化

---
+++ src/main/java/store/pengfeng/common/config/DatabaseConfig.java
@@ -0,0 +1,51 @@
+package store.pengfeng.common.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.driver-class-name:}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url:}")
+    private String url;
+
+    @Value("${spring.datasource.username:}")
+    private String userName;
+
+    @Value("${spring.datasource.password:}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDataSourceClassName(driverClassName);
+        config.addDataSourceProperty("url", url);
+        config.addDataSourceProperty("user", userName);
+        config.addDataSourceProperty("password", password);
+
+        // MySQL optimizations, see
+        // https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
+        if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource".equals(driverClassName)) {
+            config.addDataSourceProperty("cachePrepStmts", true);
+            config.addDataSourceProperty("prepStmtCacheSize", 250);
+            config.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
+            /*config.addDataSourceProperty("useServerPrepStmts",true);
+            config.addDataSourceProperty("useLocalSessionState",true);
+            config.addDataSourceProperty("rewriteBatchedStatements",true);
+            config.addDataSourceProperty("cacheResultSetMetadata",true);
+            config.addDataSourceProperty("cacheServerConfiguration",true);
+            config.addDataSourceProperty("elideSetAutoCommits",true);
+            config.addDataSourceProperty("maintainTimeStats",false);*/
+        }
+
+        return new HikariDataSource(config);
+    }
+}
--- src/main/java/store/pengfeng/common/utils/RedisUtils.java
+++ src/main/java/store/pengfeng/common/utils/RedisUtils.java
@@ -1,4 +1,4 @@
-package store.pengfeng.common.config;
+package store.pengfeng.common.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;

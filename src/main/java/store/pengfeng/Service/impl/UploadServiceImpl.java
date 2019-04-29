6bd94e2713eea5eb17b0f8624201fe1c5e552745
彭烽 <pengfeng@rayootech.com>
2019-04-29 15:22:16 +0800

    add Constant

--- src/main/java/store/pengfeng/Service/impl/UploadServiceImpl.java
+++ src/main/java/store/pengfeng/Service/impl/UploadServiceImpl.java
@@ -14,6 +14,9 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
+import static store.pengfeng.common.Constant.CONFIGFEIL;
+import static store.pengfeng.common.Constant.SERVICEHOST;
+
 @Service
 public class UploadServiceImpl implements UploadService {
 
@@ -29,7 +32,7 @@ public class UploadServiceImpl implements UploadService {
             }
             byte[] bytes = byteArrayOutputStream.toByteArray();
 
-            String filePath = new ClassPathResource("fdfs_client.conf").getFile().getAbsolutePath();
+            String filePath = new ClassPathResource(CONFIGFEIL).getFile().getAbsolutePath();
             // 1、加载配置文件，配置文件中的内容就是 tracker 服务的地址。
             ClientGlobal.init(filePath);
             // 2、创建一个 TrackerClient 对象。直接 new 一个。
@@ -37,13 +40,12 @@ public class UploadServiceImpl implements UploadService {
             // 3、使用 TrackerClient 对象创建连接，获得一个 TrackerServer 对象。
             TrackerServer trackerServer = trackerClient.getConnection();
             // 5、创建一个 StorageClient 对象，需要两个参数 TrackerServer 对象、StorageServer 的引用
-            StorageServer storageServer = null;
-            StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+            StorageClient storageClient = new StorageClient(trackerServer, null);
 
             String[] strings = storageClient.upload_file(bytes, type, null);
             List<String> list = Arrays.asList(strings);
             UploadVO uploadVO = new UploadVO();
-            String url = "http://47.100.206.217/" + list.get(0) + "/" + list.get(1);
+            String url = SERVICEHOST + list.get(0) + "/" + list.get(1);
             uploadVO.setUrl(url);
             return ResultGenerator.success(uploadVO);
         } catch (Exception e){
---
+++ src/main/java/store/pengfeng/common/Constant.java
@@ -0,0 +1,8 @@
+package store.pengfeng.common;
+
+public class Constant {
+
+    public static final String CONFIGFEIL = "fdfs_client.conf";
+    public static final String SERVICEHOST = "http://47.100.206.217/";
+
+}
